ba4ae57368b1bcc3b654fe03b1698d3a18fd62e1
Simon Henes <simon.henes@live.de>
2023-11-20 12:49:46 +0100

    Create part_1.py

---
+++ Flask_Tut/part_1.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect, url_for
+
+
+# definieren der App
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hello! This is the main page <h1>HELLO<h1> '
+
+@app.route('/<name>')
+def user(name):
+    return f'Hello {name}!'
+
+
+# mit redirect können routen auf andere routen umgeleitet werden
+# mit url_for muss nicht die url übergeben werden, sondern nur der name der Funktion!
+@app.route('/admin')
+def admin():
+    return redirect(url_for('home'))
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
