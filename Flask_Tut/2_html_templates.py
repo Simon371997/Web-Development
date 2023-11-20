f54b39ef23368705978e59ba8c3d08b09a944977
Simon Henes <simon.henes@live.de>
2023-11-20 15:21:26 +0100

    basics

---
+++ Flask_Tut/2_html_templates.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'This ist the Homepage'
+
+@app.route('/<name>')
+def user(name):
+    return f'Hello {name}!'
+
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for('home'))
+
+
+
+
+if __name__ == '__main__':
+    app.run()
