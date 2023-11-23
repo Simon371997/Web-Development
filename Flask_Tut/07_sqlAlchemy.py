a28dfd79f9f5c8abb3a38019f3a85f4cd2071063
Simon Henes <simon.henes@live.de>
2023-11-23 15:09:33 +0100

    field for mail

--- Flask_Tut/07_sqlAlchemy.py
+++ Flask_Tut/07_sqlAlchemy.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 from datetime import timedelta
+import sqlalchemy
 
 app = Flask(__name__)
 app.secret_key = "hello"
@@ -31,15 +32,25 @@ def login():
 def logout():
     flash('You have been logged out!', category='info')
     session.pop('user', None)
+    session.pop('emial', None)
     return redirect(url_for('login'))
 
 
 
-@app.route('/user')
+@app.route('/user', methods = ['POST', 'GET'])
 def user():
+    email = None
     if 'user' in session:
         user = session['user']
-        return render_template('06_user.html', user=user)
+
+        if request.method == 'POST':
+            email = request.form['email']
+            session['email'] = email
+        else:
+            if 'email' in session:
+                email = session['email']
+
+        return render_template('06_user.html', email=email)
     else:
         flash('You are not logged in!', category='warning')
         return redirect(url_for('login'))
